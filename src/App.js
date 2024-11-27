d385a2d9abc1d81d03197181c1294e2bd693931c
brennosenac07 <brennoelimar07@gmail.com>
2024-11-26 22:00:42 -0300

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React from 'react'; 
-import './App.css'; 
-import { Navbar, NavbarBrand } from 'reactstrap'; // Correct import
+import './App.css';
+import React, { useState } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
-function App() { 
-  return ( 
-    <div className="App"> 
-      <Navbar dark color="primary"> 
-        <div className="container"> 
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand> 
-          <div>Aluno: Brenno Elimar </div> 
-        </div> 
-      </Navbar> 
-      <Menu />
-    </div> 
-  ); 
+function App() {
+  const [dishes] = useState(DISHES);
+
+  return (
+    <div>
+      <Navbar dark color="primary" expand="md">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <div>Aluno: Brenno Elimar </div>
+        </div>
+      </Navbar>
+      <Menu dishes={dishes} />
+    </div>
+  );
 }
 
 export default App;
